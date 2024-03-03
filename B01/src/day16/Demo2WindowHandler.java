43993558d191305022b2b456471503c41903ce3a
delsonroy <delson.geo@gmail.com>
2024-03-03 17:08:02 -0500

    add package 17

---
+++ B01/src/day16/Demo2WindowHandler.java
@@ -0,0 +1,12 @@
+package day16;
+
+public class Demo2WindowHandler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git Push Trial");
+
+	}
+
+}
