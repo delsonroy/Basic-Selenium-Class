34a92e311a498c8b60a03351435458c81a548b94
delsonroy <delson.geo@gmail.com>
2024-03-02 21:00:37 -0500

    update the class with sysout

--- B01/src/day16/Demo1.java
+++ B01/src/day16/Demo1.java
@@ -3,7 +3,9 @@ package day16;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	System.out.println("new changes have been made");
+	
 
 	}
 
